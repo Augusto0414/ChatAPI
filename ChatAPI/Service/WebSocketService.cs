e5ee338a9c03a83c2018ddccf7ec9c045dd1b000
Augusto1404 <augustopelis4@gmail.com>
2025-01-22 18:38:54 -0500

    webSocket service create

--- ChatAPI/Service/WebSocketService.cs
+++ ChatAPI/Service/WebSocketService.cs
@@ -1,6 +1,7 @@
 ﻿using ChatAPI.Interfaces;
-using ChatAPI.Repositorio;
+using ChatAPI.Models;
 using System.Net.WebSockets;
+using System.Text;
 
 namespace ChatAPI.Service
 {
@@ -13,6 +14,42 @@ namespace ChatAPI.Service
         {
             _message = message;
         }
+        public async Task ConnectUser(string userName, WebSocket socket) 
+        {
+            _connectedUsers[userName] = socket;
+            await ReceiveMessagesAsync(userName ,socket);
+        }
 
+        private async Task ReceiveMessagesAsync(string sender, WebSocket webSocket) {
+
+            var buffer = new byte[1024 * 4];
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    _connectedUsers.Remove(sender);
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Conexión cerrada", CancellationToken.None);
+                    return; 
+                }
+                    var messageJson = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    var message = System.Text.Json.JsonSerializer.Deserialize<Mensaje>(messageJson);
+
+                    if (_connectedUsers.TryGetValue(message.ReceiverId, out var receiverSocket) &&
+                        receiverSocket.State == WebSocketState.Open)
+                    {
+                        // Forward the message to the receiver
+                        var forwardMessage = Encoding.UTF8.GetBytes(messageJson);
+                        await receiverSocket.SendAsync(new ArraySegment<byte>(forwardMessage), WebSocketMessageType.Text, true, CancellationToken.None);
+                    }
+                    else
+                    {
+                        // Save the message to the database if the receiver is not online
+                        await _message.SaveMesaje(message);
+                    }
+                
+            }
+        }
     }
-}
+ }
+
